4865e84b37edebf02a727a6359b4fb36668861bc
Fanny <fanny20ti@mahasiswa.pcr.ac.id>
2024-05-19 15:43:52 +0700

    fix responsive

--- Assets/Scripts/ReadManager.cs
+++ Assets/Scripts/ReadManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -11,6 +12,8 @@ public class ReadManager : MonoBehaviour
     public TMP_Text learnDesc;
     public GameObject textContentPlace;
     public TMP_Text textContent;
+    public GameObject textViewPort;
+    public GameObject panel;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +39,17 @@ public class ReadManager : MonoBehaviour
         // Adjust the height of the RectTransform
         RectTransform rectTransform = textContent.GetComponent<RectTransform>();
         rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, preferredHeight);
+
+        // Adjust the height of the RectTransform of the viewport
+        RectTransform viewportRectTransform = textViewPort.GetComponent<RectTransform>();
+        float panelHeight = panel.GetComponent<RectTransform>().rect.height;
+        float parentHeight = viewportRectTransform.parent.GetComponent<RectTransform>().rect.height;
+
+        // Calculate the offset needed to align the bottom of the viewport with the bottom of the panel
+        float bottomOffset = (parentHeight - panelHeight) + 50f;
+
+        viewportRectTransform.offsetMin = new Vector2(viewportRectTransform.offsetMin.x, bottomOffset);
+
     }
 
     public void PlayTest()
